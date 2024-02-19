914045daf04597d9c9ca4ee1123664f494773412
João Paulo Pereira <joaopaulop.dev@gmail.com>
2024-02-18 22:21:04 -0300

    first commit

---
+++ 01TiposPrimitivos/typeNumber.js
@@ -0,0 +1,32 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+
+// + (Adição) - (Subtração) * (Multiplicação) / (Divisão)
+
+console.log(operacaoMatematica);
+
+//Number Ponto flutuante
+const numPontoFlutuante = 3.3;
+
+const num1 = .5;
+const num2 = 1.3;
+
+const novaOperacao= num1+num2;
+
+console.log(novaOperacao)
+
+
+// NaN --> Not A Number (Não é Um Número)
+
+const naoNumero = "João";
+
+
+console.log(num1*naoNumero);
